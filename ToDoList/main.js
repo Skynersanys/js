a637b58d28a57ba910b4a56636b77eb9f5b034aa
Alexandr <cantrabelarus@gmail.com>
2017-11-13 02:08:52 +0300

    new js and animation

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,31 +1,39 @@
 var mas = [];
+var addtext = document.getElementById("button");
 
-  var addtext = document.getElementById("button");
-
-  addtext.onclick = function() {
+addtext.onclick = function()
+  {
+    var valueform = document.getElementById('form');
     var text = document.getElementById('form').value;
     if(text != "")
     {
         mas.push(text);
         newwork();
+        valueform.value = "";
     }
-    else {
-      text.innerHTML("Введите задание");
+    else
+    {
+      valueform.value = "Введите задание";
     }
-}
+  }
 
-function newwork() {
+function newwork()
+{
   var i = 0;
-    var newstring = document.getElementById('newtext');
+  var newstring = document.getElementById('newtext');
   var tempstr = [];
-  mas.forEach(function(text){
-    tempstr += "<button class=delete onclick=deletework(" + i + ")>x</button><span>" + text + "</span><br>";
+  mas.forEach(function(text)
+  {
+    tempstr += "<button class=delete onclick=deletework(" + i + ")><img class=image_delete src="+"images/delete.png"+"></button><span>" + text + "</span><br>";
     newstring.innerHTML = tempstr[i];
     i++;
   });
   newstring.innerHTML = tempstr;
+
 }
-function deletework(i){
+
+function deletework(i)
+{
   mas.splice(i, 1);
   newwork();
 }
